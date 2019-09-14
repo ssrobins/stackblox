b209b8aec041c6235b83e780aadc9acc6c31e88a
Steve Robinson <ssrobins@gmail.com>
2019-09-14 13:50:31 -0700

    Specify the recipe revision of ssrobins_engine

--- conanfile.py
+++ conanfile.py
@@ -6,4 +6,4 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def requirements(self):
-        self.requires.add("ssrobins_engine/0.1.0")
+        self.requires.add("ssrobins_engine/0.1.0#f678e48bb74e5231a29056bb9e3d1fe8d1235425")
