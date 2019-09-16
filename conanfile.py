00d2c82d6fa353463a0e9fd3983e5fdbf21d6a30
Steve Robinson <ssrobins@gmail.com>
2019-09-15 23:36:17 -0600

    Add gtest because it's no longer an ssrobins_engine package dependency. Update ssrobins_engine to bring in that plus changes to packaging copy

--- conanfile.py
+++ conanfile.py
@@ -6,4 +6,5 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def requirements(self):
-        self.requires.add("ssrobins_engine/0.1.0#f678e48bb74e5231a29056bb9e3d1fe8d1235425")
+        self.requires.add("gtest/1.8.1#0ab3cafdcd7fe85deaefe41b3011d36fbd86e38e")
+        self.requires.add("ssrobins_engine/0.1.0#fd9f76ce040cba32fc6e47149b55ddb655da90dd")
