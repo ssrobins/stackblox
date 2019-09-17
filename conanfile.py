117059d65beb5e8311feb8c3c53d0ad25b9a444c
Steve Robinson <ssrobins@gmail.com>
2019-09-17 11:19:53 -0600

    Update ssrobins_engine package to downgrade Gradle build tools to work around a build problem in Android Studio

--- conanfile.py
+++ conanfile.py
@@ -8,4 +8,4 @@ class Conan(ConanFile):
 
     def requirements(self):
         self.requires.add("gtest/1.8.1#0ab3cafdcd7fe85deaefe41b3011d36fbd86e38e")
-        self.requires.add("ssrobins_engine/0.1.0#fd9f76ce040cba32fc6e47149b55ddb655da90dd")
+        self.requires.add("ssrobins_engine/0.1.0#fd90aa57c604024d1e49597f75616ad42c7683a1")
