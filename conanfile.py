812eb5e95b51545ca4c2fa5ff2b7943f7c7bc354
Steve Robinson <ssrobins@gmail.com>
2019-10-02 19:13:03 -0700

    Upgrade package to get the latest gradle build tools

--- conanfile.py
+++ conanfile.py
@@ -8,4 +8,4 @@ class Conan(ConanFile):
 
     def requirements(self):
         self.requires.add("gtest/1.8.1#87754ca5593a1001e629122ee514d931aa9e8bf7")
-        self.requires.add("ssrobins_engine/0.1.0#b89fc804e84394548b432a2912fd2d567f4ac2e8")
+        self.requires.add("ssrobins_engine/0.1.0#518a3143d2536d48691a4c3c2520e34bbfca8aae")
