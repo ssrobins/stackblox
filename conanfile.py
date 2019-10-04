9b2bc151298f057436bf98f795c4213811a45dd2
Steve Robinson <ssrobins@gmail.com>
2019-10-04 16:42:01 -0700

    Upgrade Google Test

--- conanfile.py
+++ conanfile.py
@@ -7,5 +7,5 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def requirements(self):
-        self.requires.add("gtest/1.8.1#87754ca5593a1001e629122ee514d931aa9e8bf7")
+        self.requires.add("gtest/1.10.0#016389183489f5c0a5db55001303991db14e7ce1")
         self.requires.add("ssrobins_engine/0.1.0#518a3143d2536d48691a4c3c2520e34bbfca8aae")
