04bf8fee45a30f39b79e16f83b0936d5463d0f51
Steve Robinson <ssrobins@gmail.com>
2019-11-03 16:59:35 -0800

    Use new Android Gradle and cmake-utils packages and upgrade the other packages

--- conanfile.py
+++ conanfile.py
@@ -7,5 +7,8 @@ class Conan(ConanFile):
     generators = "cmake"
 
     def requirements(self):
-        self.requires.add("gtest/1.10.0#016389183489f5c0a5db55001303991db14e7ce1")
-        self.requires.add("ssrobins_engine/0.1.0#bfdd7e61f2ae28bfee82720e4b9e9b39ff8da27d")
+        if self.settings.os == "Android":
+            self.requires.add("android_sdl2/0.1.0#99ca5d8f13de93355ccabef47eb90c6343d800f7")
+        self.requires.add("cmake_utils/0.1.0#9efedba7676c460adbfbf3a98e4c51cea7d320db")
+        self.requires.add("gtest/1.10.0#ca07d9d05d12f3bbb091f095e30f7c54cd2920ee")
+        self.requires.add("ssrobins_engine/0.2.0#012ac83390f32170f0438c909972c837931eb297")
