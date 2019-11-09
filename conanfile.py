36c1eaddca315f335e8816a1742c9d298e56b080
Steve Robinson <ssrobins@gmail.com>
2019-11-09 14:11:53 -0800

    Upgrade Gradle

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires.add("android_sdl2/0.1.0#99ca5d8f13de93355ccabef47eb90c6343d800f7")
+            self.build_requires.add("android_sdl2/0.1.0#db746d43b1b7e728db3b2d280aae273da3922e68")
         self.build_requires.add("cmake_utils/0.1.0#9efedba7676c460adbfbf3a98e4c51cea7d320db")
 
     def requirements(self):
