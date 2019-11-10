d4725da69fbf5542dd992a49fdab155390beaf47
Steve Robinson <ssrobins@gmail.com>
2019-11-10 10:17:28 -0800

    Fix missing assets in the installs on non-Apple platforms

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires.add("android_sdl2/0.1.0#db746d43b1b7e728db3b2d280aae273da3922e68")
-        self.build_requires.add("cmake_utils/0.1.0#9efedba7676c460adbfbf3a98e4c51cea7d320db")
+        self.build_requires.add("cmake_utils/0.1.0#89a9bc6923fea4dabf19de6aa6b7ddcf8667b2ee")
 
     def requirements(self):
         self.requires.add("gtest/1.10.0#ca07d9d05d12f3bbb091f095e30f7c54cd2920ee")
