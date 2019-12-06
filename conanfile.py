9fc48731c9025b75bc67ac9feb1de8442b041fce
Steve Robinson <ssrobins@gmail.com>
2019-12-05 18:47:28 -0800

    Upgrade Android Studio and Gradle

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires.add("android_sdl2/0.1.0#db746d43b1b7e728db3b2d280aae273da3922e68")
+            self.build_requires.add("android_sdl2/0.1.0#debda07d9c722b256dc83beccc487d30fffbd274")
         self.build_requires.add("cmake_utils/0.2.0#9bd58176d9770733cb8d216a9b35017ae0ee6689")
 
     def requirements(self):
