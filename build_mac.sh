e9b487c0d65bfeaf5db6e4202fdc8e24c599eff5
Steve Robinson <ssrobins@gmail.com>
2019-12-21 21:39:30 +0000

    Run Conan inside CMake. Add CMakeSettings.json so the project folder can be opened in VS2019.

--- build_mac.sh
+++ build_mac.sh
@@ -6,10 +6,6 @@ cd $(dirname "$0")
 mkdir -p $build_dir
 cd $build_dir
 
-conan remote add bintray-stever https://api.bintray.com/conan/stever/conan --insert --force
-
-conan install --update .. -s compiler.version=11.0
-
 config=Release
 
 cmake -G Xcode ..
--- build_windows.bat
+++ build_windows.bat
@@ -7,15 +7,11 @@ cd /d %~dp0
 if not exist %build_dir% mkdir %build_dir%
 cd %build_dir%
 
-conan remote add bintray-stever https://api.bintray.com/conan/stever/conan --insert --force || goto :error
-
-conan install --update .. -s arch=x86 -s compiler.version=16 -s compiler.runtime=MT || goto :error
-
 cmake -G "Visual Studio 16 2019" -A Win32 .. || goto :error
 
 set config=Release
 
-cmake --build . --config %config% -- /m || goto :error
+cmake --build . --config %config% --verbose -- /m || goto :error
 
 ctest -C %config% --output-on-failure || goto :error
 
