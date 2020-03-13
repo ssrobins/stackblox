5b1075df4484b1fbd0711328d5d290fc35113932
Steve Robinson <ssrobins@gmail.com>
2020-03-12 22:47:04 -0700

    Upgrade Conan packages

--- conanfile.py
+++ conanfile.py
@@ -8,9 +8,9 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#6bcfa9bb0ef5adf28594b838a718ed2e8ce90208")
-        self.build_requires("cmake_utils/0.3.1#7b308615a235fdf046db096dd35325c0375c2f88")
+            self.build_requires("android_sdl2/0.1.0#85f60caeb1c67dda9309809530c85f60a781b916")
+        self.build_requires("cmake_utils/0.3.1#bddead915084dbfac2b4114574fae5ae8545e540")
 
     def requirements(self):
-        self.requires("gtest/1.10.0#fbedb03a0bb64f1df48a54f4394eab3978d6ffb3")
-        self.requires("ssrobins_engine/0.2.0#bf865cfccef9adeb95fd387b723c796adb0cfcdc")
+        self.requires("gtest/1.10.0#1c931c5867b5491b08d404971e9171cefb8188a5")
+        self.requires("ssrobins_engine/0.2.0#981c6feef81508d8359b065867fb3e2959dbb43a")
