7aa44d1696362b25d88e8765421592adb2942418
Steve Robinson <ssrobins@gmail.com>
2020-03-28 00:35:54 -0700

    Get assets folder copied to the proper location for mult-config Ninja Android builds

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/0.1.0#1aea09bd255829b7b78634e74c7fd296d99e4582")
-        self.build_requires("cmake_utils/0.3.1#d093c585be2418d6a664babbec39e71d6b5cd11d")
+        self.build_requires("cmake_utils/0.3.1#b92e3b563e31a4fe0e55849f3bfdb55eb7b06284")
 
     def requirements(self):
         self.requires("gtest/1.10.0#1dc34103486deb0c4056a3326c005456f805fc6b")
