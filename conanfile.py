79768d477ec95f6efd386ef677923a029dbf8edc
Steve Robinson <ssrobins@gmail.com>
2020-09-29 21:12:26 -0700

    Update Conan package to use new binary package filenames

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/0.1.0#33d7832e580dafa05810540f2808f3b070286189")
-        self.build_requires("cmake_utils/0.3.1#217df94bdd79aaa9b2aec1a8bd8b4eca73411f25")
+        self.build_requires("cmake_utils/0.3.1#30236679ffe9804515e8c092a744560a72ffb55a")
 
     def requirements(self):
         self.requires("gtest/1.10.0#af5a75e307b6039ab662434cb838441f1342fb51")
