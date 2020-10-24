d48f5e19eac22ca50565374e95cb030e8c55a7d3
Steve Robinson <ssrobins@gmail.com>
2020-10-23 23:32:50 -0700

    Update package

--- conanfile.py
+++ conanfile.py
@@ -13,4 +13,4 @@ class Conan(ConanFile):
 
     def requirements(self):
         self.requires("gtest/1.10.0#044b450dd54106b2b6a32a6fa46ba984756f73b6")
-        self.requires("ssrobins_engine/0.2.0#076e3307a38c5f0c32a67c93668258b4fb45d7fd")
+        self.requires("ssrobins_engine/0.2.0#22a24d4ec36b086c751869fac7faa3ef6ce996ab")
