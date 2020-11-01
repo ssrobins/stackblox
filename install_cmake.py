bcf903ab730375e77c1ae9531519907dcb413a55
Steve Robinson <ssrobins@gmail.com>
2020-10-31 22:07:18 -0700

    Try python install script for CMake

---
+++ install_cmake.py
@@ -0,0 +1,41 @@
+#!/usr/bin/env python3
+
+import platform
+import subprocess
+import tarfile
+import urllib.request
+import zipfile
+
+
+cmake_version = "3.19.0-rc2"
+
+if platform.system() == "Darwin":
+    cmake_platform = "Darwin-x86_64"
+    cmake_archive_ext = ".tar.gz"
+elif platform.system() == "Linux":
+    cmake_platform = "Linux-x86_64"
+    cmake_archive_ext = ".tar.gz"
+elif platform.system() == "Windows":
+    cmake_platform = "win64-x64"
+    cmake_archive_ext = ".zip"
+
+cmake_dir = f"cmake-{cmake_version}-{cmake_platform}"
+cmake_archive = f"{cmake_dir}{cmake_archive_ext}"
+
+urllib.request.urlretrieve(
+    f"http://dnqpy.com/temp/{cmake_archive}",
+    f"{cmake_archive}"
+)
+
+if "tar" in cmake_archive:
+    with tarfile.open(f"{cmake_archive}", "r:gz") as tar:
+        tar.extractall()
+elif "zip" in cmake_archive:
+    with zipfile.ZipFile(f"{cmake_archive}", 'r') as zip:
+        zip.extractall()
+    
+else:
+    print("Unsupported archive: {cmake_archive}")
+    exit(1)
+
+subprocess.run("cmake --version", shell=True, check=True)
