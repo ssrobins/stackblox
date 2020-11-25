dfba8b4516bc92e95d901c139d98d86cc51a228c
Steve Robinson <ssrobins@gmail.com>
2020-11-24 23:02:03 -0800

    Upgrade conan-ssrobins_engine to fix build errors after MSVC upgrade

--- conanfile.py
+++ conanfile.py
@@ -8,9 +8,9 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#080a2227eb3904d6c6df6bd90ff689d1cf12736e")
+            self.build_requires("android_sdl2/0.1.0#b185e9d8c14eef5fcbce6e9fb0468c944149a4c5")
         self.build_requires("cmake_utils/0.3.1#cc144db607f04d12c0b18303a7c7d37386ce0783")
 
     def requirements(self):
-        self.requires("gtest/1.10.0#044b450dd54106b2b6a32a6fa46ba984756f73b6")
-        self.requires("ssrobins_engine/0.2.0#22a24d4ec36b086c751869fac7faa3ef6ce996ab")
+        self.requires("gtest/1.10.0#30c3a8ee7b1c80b95c376542109b72102c191bcd")
+        self.requires("ssrobins_engine/0.2.0#fc3b0de476823df5833e6c727aacf8823e23dfb3")
