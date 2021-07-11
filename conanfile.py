1599feebeb584ea484b204d7185b3988e097d03c
Steve Robinson <ssrobins@gmail.com>
2021-07-10 23:33:22 -0700

    Use new linux and android docker images

--- conanfile.py
+++ conanfile.py
@@ -8,9 +8,9 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#a8e9c63ad2763313865f84a027b0509ab6a4208f")
-        self.build_requires("cmake_utils/0.3.1#da30d52b2c5db13fc90a22140f704d67c7635319")
+            self.build_requires("android_sdl2/0.1.0#d299a2e1a9b1b779eb0e2bd7c11d2cf845e5f403")
+        self.build_requires("cmake_utils/0.3.1#77d5f06b9b20302a5410e41ed45e7bbea7de90a5")
 
     def requirements(self):
-        self.requires("gtest/1.10.0#a2327ae0fc8d73335445e3951ae998a6f0887be4")
-        self.requires("ssrobins_engine/0.3.0#7744a6a9ab567a8caa242397b15714d2b16a2441")
+        self.requires("gtest/1.11.0#1b8ec1a4d85a679be150cd3ad2dad866ca2aab30")
+        self.requires("ssrobins_engine/0.3.0#697927c3370b68c5673304ed9915b1e38bf8e669")
