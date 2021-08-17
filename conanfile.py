2cdc282105e193336ece60abca8588d2a39d3a7c
Steve Robinson <ssrobins@gmail.com>
2021-08-16 23:27:24 -0700

    Upgrade Android NDK

--- conanfile.py
+++ conanfile.py
@@ -12,5 +12,5 @@ class Conan(ConanFile):
         self.build_requires("cmake_utils/0.3.1#77d5f06b9b20302a5410e41ed45e7bbea7de90a5")
 
     def requirements(self):
-        self.requires("gtest/1.11.0#e16a50c3a5d1fcb9955b759f6b6d82dea712d821")
-        self.requires("ssrobins_engine/0.3.0#4c16af68030be3ef1ad1b9eb0a139e58c1449886")
+        self.requires("gtest/1.11.0#5c7586ef6610e8053ee79086fbc7743d9fbde653")
+        self.requires("ssrobins_engine/0.3.0#3b2b885d3bfc96b00e7c3c69fdd2282f02f3bde3")
