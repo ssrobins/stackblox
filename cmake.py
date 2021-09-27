5b8cf07898fe965bc0f4b2a60c6e431caacd49d5
Steve Robinson <ssrobins@gmail.com>
2021-09-26 22:43:19 -0700

    Conan packages will be built if missing, except in automated builds

--- cmake.py
+++ cmake.py
@@ -18,6 +18,7 @@ def main():
     parser.add_argument("platform", choices=platform, help="Build platform")
     parser.add_argument("--config", help="Build config")
     parser.add_argument("--build", action="store_true", help="Run build in CMake")
+    parser.add_argument("--noConanPkgBuild", action="store_true", help="Use pre-built Conan packages and fail if they don't exist")
     parser.add_argument("--test", action="store_true", help="Run tests in CTest")
     parser.add_argument("--package", action="store_true", help="Run packaging in CPack")
     command_args = parser.parse_args()
@@ -29,7 +30,11 @@ def main():
     else:
         config = "Debug"
 
-    subprocess.run(f"cmake --preset={command_args.platform}",
+    conan_build_option = str()
+    if command_args.noConanPkgBuild:
+        conan_build_option = "-DCONAN_BUILD_MISSING_PKGS=OFF"
+
+    subprocess.run(f"cmake --preset={command_args.platform} {conan_build_option}",
         cwd=script_path, shell=True, check=True)
 
     if command_args.build:
