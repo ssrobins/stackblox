465ab91eda96560703f950dd1ec6778be7659bf4
Steve Robinson <ssrobins@gmail.com>
2021-11-13 19:14:26 -0800

    Add Bluetooth descriptions to satisfy iOS App Store submission requirements

--- conanfile.py
+++ conanfile.py
@@ -8,9 +8,9 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/0.1.0#44ad36020b2075dc63f69d8f968dbe81a452bfa8")
-        self.build_requires("cmake_utils/0.3.1#2e2e0de4635e8c3a83655a207c63ad34ba1dadd7")
+            self.build_requires("android_sdl2/1.0.0#33a5929ed7796027107fb57187ae4248e0faf3ce")
+        self.build_requires("cmake_utils/1.0.0#926ac6b58f4f500187b0a004491f5d0f7ebe0ed4")
 
     def requirements(self):
-        self.requires("gtest/1.11.0#da27a76e3c6d8034aee23541c0c4e9720f224ecc")
-        self.requires("ssrobins_engine/0.3.0#8f04c86eafae2ff0e33063296233d79e48c9a701")
+        self.requires("gtest/1.11.0#d87db002fb4324adf3f0a00fcf1e46b306d6271a")
+        self.requires("ssrobins_engine/0.3.0#4a6aaeeaae1931812370a21c950d2eb6a004ae12")
