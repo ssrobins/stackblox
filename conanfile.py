743256c283fc60a5b6c58323873cd894875c2e49
Steve Robinson <ssrobins@gmail.com>
2021-11-21 18:09:29 -0800

    Use Ninja-built macOS and iOS packages

--- conanfile.py
+++ conanfile.py
@@ -9,8 +9,8 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/1.0.0#33a5929ed7796027107fb57187ae4248e0faf3ce")
-        self.build_requires("cmake_utils/2.0.1#bc87acc9a67867fb20e22e3c51eb4c070a9f9758")
+        self.build_requires("cmake_utils/3.0.0#4e7b4d9bfca394477325cdfc8eacce8b1c82583e")
 
     def requirements(self):
-        self.requires("gtest/1.11.0#8fd6a2a8d5711b4ddb33e9addc96ab35cdb9f538")
-        self.requires("ssrobins_engine/1.0.0#d220df5c247a38aef42cd86471ffaef0aae4d726")
+        self.requires("gtest/1.11.0#7a8dc4022cf1753b71057bfe89043cf4a5b4dc8f")
+        self.requires("ssrobins_engine/1.0.0#6de66ab2a4a7269def4a08fa8be75f6b5cd89deb")
