127c568d4c8ddedbbd77092df337df6a466327b1
Steve Robinson <ssrobins@gmail.com>
2021-11-26 18:33:51 -0800

    Fix Android app startup error and upgrade target SDK to 31

--- conanfile.py
+++ conanfile.py
@@ -8,8 +8,8 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/1.0.0#33a5929ed7796027107fb57187ae4248e0faf3ce")
-        self.build_requires("cmake_utils/3.0.0#4e7b4d9bfca394477325cdfc8eacce8b1c82583e")
+            self.build_requires("android_sdl2/2.1.0#990c9e26dd59ee78d2fe5d1c3e2f1c544afe1dfd")
+        self.build_requires("cmake_utils/4.0.0#6f1fbd5e82af9782f3cb28a040dfe34c6e5c990a")
 
     def requirements(self):
         self.requires("gtest/1.11.0#7a8dc4022cf1753b71057bfe89043cf4a5b4dc8f")
