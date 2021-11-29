b9c6041a36edf1b00f7c6d38af5ccd4bf43a917a
Steve Robinson <ssrobins@gmail.com>
2021-11-28 17:59:24 -0800

    Use Conan package for assets instead of storing them in the repo (#11)
    * Remove assets
    
    * Update asset paths to reference conan packages
    
    * Upgrade product version
    
    * Move Android icon copy into app so I can specify the new asset location

--- conanfile.py
+++ conanfile.py
@@ -9,8 +9,9 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/2.1.0#990c9e26dd59ee78d2fe5d1c3e2f1c544afe1dfd")
-        self.build_requires("cmake_utils/4.0.0#6f1fbd5e82af9782f3cb28a040dfe34c6e5c990a")
+        self.build_requires("cmake_utils/5.0.0#1ecfed8c68a43ea17d321701cc8a91df21d06453")
 
     def requirements(self):
         self.requires("gtest/1.11.0#7a8dc4022cf1753b71057bfe89043cf4a5b4dc8f")
         self.requires("ssrobins_engine/1.0.0#6de66ab2a4a7269def4a08fa8be75f6b5cd89deb")
+        self.requires("stackblox_assets/1.0.0#7e796301888eccdfe557d90cfa53f6dff5ab97e7")
