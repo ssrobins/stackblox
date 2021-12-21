2d3743d9047f134b9d8c51acea5afe260c213fcb
Steve Robinson <ssrobins@gmail.com>
2021-12-20 23:35:46 -0800

    Fix Gradle sync error in Android Studio

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/2.1.0#4631baf96c5937b22753e9ac1146265ca3b32aff")
+            self.build_requires("android_sdl2/2.3.0#9517496737c184f98924eec823064964c8123474")
         self.build_requires("cmake_utils/5.1.0#2c0d8f9dda3cac137976849bb3851fd6c4999de0")
 
     def requirements(self):
