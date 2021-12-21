b2128e5a0ecc11d6de8b824e416a47829e8829ea
Steve Robinson <ssrobins@gmail.com>
2021-12-21 12:16:09 -0800

    Upgrade ssrobins_engine

--- conanfile.py
+++ conanfile.py
@@ -13,5 +13,5 @@ class Conan(ConanFile):
 
     def requirements(self):
         self.requires("gtest/1.11.0#8ef69248cd41cd3a29dcfd3340f0589a291c2631")
-        self.requires("ssrobins_engine/1.2.0")
+        self.requires("ssrobins_engine/1.2.0#f5f260275e36f6cc278bb32787ab24d01b8b1356")
         self.requires("stackblox_assets/1.1.0#d2ab7c56b39064b4ff03ab69e5ff9cf0995bfeb1")
