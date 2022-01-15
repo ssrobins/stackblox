569188cbb19cca4f254faa631ba18dc3d77eda97
Steve Robinson <ssrobins@gmail.com>
2022-01-15 11:48:21 -0800

    Use asset catalog for macOS icons (#15)
    * Configure macOS to use xcassets for the icons
    
    * Update product version

--- conanfile.py
+++ conanfile.py
@@ -8,10 +8,10 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/2.3.0#9517496737c184f98924eec823064964c8123474")
-        self.build_requires("cmake_utils/5.1.0#2c0d8f9dda3cac137976849bb3851fd6c4999de0")
+            self.build_requires("android_sdl2/2.4.0#41295dd86e5d152269019ea03930766db0c0bddc")
+        self.build_requires("cmake_utils/6.0.0#ac1a71244046f44f5484a554918f6a42a1f86d99")
 
     def requirements(self):
         self.requires("gtest/1.11.0#8ef69248cd41cd3a29dcfd3340f0589a291c2631")
         self.requires("ssrobins_engine/1.2.0#f5f260275e36f6cc278bb32787ab24d01b8b1356")
-        self.requires("stackblox_assets/1.1.0#d2ab7c56b39064b4ff03ab69e5ff9cf0995bfeb1")
+        self.requires("stackblox_assets/2.0.0#d287a5317b98108ba2594c5ed63217990ebc138e")
