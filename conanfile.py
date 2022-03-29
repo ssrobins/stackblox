5376ba39a23b5654338dd6de7d07bdc367f0b7ed
Steve Robinson <ssrobins@gmail.com>
2022-03-28 22:43:26 -0700

    Upgrade zlib

--- conanfile.py
+++ conanfile.py
@@ -8,10 +8,10 @@ class Conan(ConanFile):
 
     def build_requirements(self):
         if self.settings.os == "Android":
-            self.build_requires("android_sdl2/2.6.0#508b1df8b9b4b37172e71af0c64acb23a413e3c9")
+            self.build_requires("android_sdl2/2.6.1#cec773c2bd9f752184188e5d43058439b4311cb4")
         self.build_requires("cmake_utils/6.1.0#9ced9bcfd95178b35d2ec5955b725a5652dbda26")
 
     def requirements(self):
         self.requires("gtest/1.11.0#8ef69248cd41cd3a29dcfd3340f0589a291c2631")
-        self.requires("ssrobins_engine/1.2.0#781bb1f65dcb2ff4f3719dc0ea24e5857606dbd5")
+        self.requires("ssrobins_engine/1.2.0#9d170ee47bc0546ccf0cea5252e8e3eeab987548")
         self.requires("stackblox_assets/2.0.0#d287a5317b98108ba2594c5ed63217990ebc138e")
