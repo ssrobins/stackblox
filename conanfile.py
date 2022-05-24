14f8e1d1f9076391b290afa5e06a50bc81c840d7
Steve Robinson <ssrobins@gmail.com>
2022-05-24 09:07:00 -0700

    Make gtest target compatible with Conan's generators that use find_package()

--- conanfile.py
+++ conanfile.py
@@ -9,7 +9,7 @@ class Conan(ConanFile):
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/2.6.2#da38ed876673a6da88d73f6dcb0138de5561125b")
-        self.build_requires("cmake_utils/7.0.0#9bf47716aeee70a8dcfc8592831a0318eb327a09")
+        self.build_requires("cmake_utils/8.0.0#b0932588b4ccd8ac8db4fed90295f7480ac0b37f")
 
     def requirements(self):
         self.requires("gtest/1.11.0#b6d68d3070e0f968f2a456728c7f7af1621d51f2")
