2586e9e0a9ce83195105e6470fcdd713b0cb8547
Steve Robinson <ssrobins@gmail.com>
2022-05-24 00:10:34 -0700

    Initial setup of using one config per Conan package

--- conanfile.py
+++ conanfile.py
@@ -3,15 +3,15 @@ from conans import ConanFile
 class Conan(ConanFile):
     name = "StackBlox"
     version = ""
-    settings = "os"
-    generators = "cmake"
+    settings = "os", "arch", "compiler", "build_type"
+    generators = "cmake", "cmake_find_package_multi"
 
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/2.6.2#da38ed876673a6da88d73f6dcb0138de5561125b")
-        self.build_requires("cmake_utils/6.1.0#9ced9bcfd95178b35d2ec5955b725a5652dbda26")
+        self.build_requires("cmake_utils/7.0.0#9bf47716aeee70a8dcfc8592831a0318eb327a09")
 
     def requirements(self):
-        self.requires("gtest/1.11.0#e4ccbfb41abf17466702bd741d880a1dd2a45fba")
-        self.requires("ssrobins_engine/1.2.0#197264673b517775154e16290712d883fe80574b")
+        self.requires("gtest/1.11.0#b6d68d3070e0f968f2a456728c7f7af1621d51f2")
+        self.requires("ssrobins_engine/1.2.0#caa17d86d186366de310ac60c337edee81ac0e9e")
         self.requires("stackblox_assets/2.0.0#d287a5317b98108ba2594c5ed63217990ebc138e")
