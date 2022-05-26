fe65bd92ed0b0552f86f175fbc5004a116f8d34a
Steve Robinson <ssrobins@gmail.com>
2022-05-25 20:45:23 -0700

    Convert to Conan's CMakeDeps generator (#19)

--- conanfile.py
+++ conanfile.py
@@ -4,12 +4,12 @@ class Conan(ConanFile):
     name = "StackBlox"
     version = ""
     settings = "os", "arch", "compiler", "build_type"
-    generators = "cmake", "cmake_find_package_multi"
+    generators = "CMakeDeps"
 
     def build_requirements(self):
         if self.settings.os == "Android":
             self.build_requires("android_sdl2/2.6.2#da38ed876673a6da88d73f6dcb0138de5561125b")
-        self.build_requires("cmake_utils/8.0.0#b0932588b4ccd8ac8db4fed90295f7480ac0b37f")
+        self.build_requires("cmake_utils/9.0.1#7f745054c87ea0007a89813a4d2c30c4c95e24b2")
 
     def requirements(self):
         self.requires("gtest/1.11.0#b6d68d3070e0f968f2a456728c7f7af1621d51f2")
