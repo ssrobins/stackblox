cb460b2e964ca35478babc11636507f4e98c1ed8
Steve Robinson <ssrobins@gmail.com>
2022-06-22 20:09:19 -0700

    Fix freetype's third-party dependencies

--- conanfile.py
+++ conanfile.py
@@ -8,7 +8,7 @@ class Conan(ConanFile):
 
     def requirements(self):
         if self.settings.os == "Android":
-            self.requires("android_sdl/2.6.2#fe249e56a871dd3e09cf9ecbdac13e775463d983")
-        self.requires("cmake_utils/10.0.1#fe249e56a871dd3e09cf9ecbdac13e775463d983")
-        self.requires("gtest/1.11.0#fe249e56a871dd3e09cf9ecbdac13e775463d983")
-        self.requires("ssrobins_engine/1.2.0#fe249e56a871dd3e09cf9ecbdac13e775463d983")
+            self.requires("android_sdl/2.6.2#f374feb3c060e091bdaf0a0005dd64c28ed04c67")
+        self.requires("cmake_utils/10.0.1#f374feb3c060e091bdaf0a0005dd64c28ed04c67")
+        self.requires("gtest/1.11.0#f374feb3c060e091bdaf0a0005dd64c28ed04c67")
+        self.requires("ssrobins_engine/1.2.0#f374feb3c060e091bdaf0a0005dd64c28ed04c67")
