5ba927a88b65f8731869ee3fc1ea0ac0a068b35e
Steve Robinson <ssrobins@gmail.com>
2022-07-02 15:37:57 -0700

    Upgrade gtest

--- conanfile.py
+++ conanfile.py
@@ -12,5 +12,5 @@ class Conan(ConanFile):
         if self.settings.os == "Android":
             self.requires("android_sdl/2.6.2@ssrobins")
         self.requires("cmake_utils/10.0.1@ssrobins")
-        self.requires("gtest/1.12.0@ssrobins")
+        self.requires("gtest/1.12.1@ssrobins")
         self.requires("ssrobins_engine/1.2.0@ssrobins")
