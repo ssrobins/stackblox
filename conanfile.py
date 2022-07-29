eb3b7904b1b57ff3837e162a915ef460d8cce7f6
Steve Robinson <ssrobins@gmail.com>
2022-07-29 12:08:11 -0700

    Go back to setting gtest as a test requirement

--- conanfile.py
+++ conanfile.py
@@ -8,9 +8,11 @@ class Conan(ConanFile):
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps"
 
+    def build_requirements(self):
+        self.test_requires("gtest/1.12.1@ssrobins")
+
     def requirements(self):
         if self.settings.os == "Android":
             self.requires("android_sdl/2.6.2@ssrobins")
         self.requires("cmake_utils/10.0.1@ssrobins")
-        self.requires("gtest/1.12.1@ssrobins")
         self.requires("ssrobins_engine/1.3.0@ssrobins")
