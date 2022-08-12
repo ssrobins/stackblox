78dcb6851083162efd9bb674b8ace9a2496eb733
Steve Robinson <ssrobins@gmail.com>
2022-08-11 23:29:13 -0700

    Fix Android APK copy and upgrade Gradle (#27)

--- conanfile.py
+++ conanfile.py
@@ -13,6 +13,6 @@ class Conan(ConanFile):
 
     def requirements(self):
         if self.settings.os == "Android":
-            self.requires("android_sdl/2.6.2@ssrobins")
-        self.requires("cmake_utils/10.0.1@ssrobins")
+            self.requires("android_sdl/2.7.0@ssrobins")
+        self.requires("cmake_utils/10.0.2@ssrobins")
         self.requires("ssrobins_engine/2.0.0@ssrobins")
