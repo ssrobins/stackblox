e068dfa617beb51977bb1e54418236eb633682ba
Steve Robinson <ssrobins@gmail.com>
2023-06-29 21:56:27 -0700

    Don't build what you don't use

--- conanfile.py
+++ conanfile.py
@@ -7,6 +7,34 @@ class Conan(ConanFile):
     version = ""
     settings = "os", "arch", "compiler", "build_type"
     generators = "CMakeDeps"
+    default_options = {
+        "sdl_image/*:bmp": False,
+        "sdl_image/*:gif": False,
+        "sdl_image/*:imageio": False,
+        "sdl_image/*:lbm": False,
+        "sdl_image/*:pcx": False,
+        "sdl_image/*:pnm": False,
+        "sdl_image/*:svg": False,
+        "sdl_image/*:tga": False,
+        "sdl_image/*:with_libjpeg": False,
+        "sdl_image/*:with_libpng": True,
+        "sdl_image/*:with_libtiff": False,
+        "sdl_image/*:with_libwebp": False,
+        "sdl_image/*:xcf": False,
+        "sdl_image/*:xpm": False,
+        "sdl_image/*:xv": False,
+        "sdl_mixer/*:flac": False,
+        "sdl_mixer/*:fluidsynth": False,
+        "sdl_mixer/*:mad": False,
+        "sdl_mixer/*:mikmod": False,
+        "sdl_mixer/*:modplug": False,
+        "sdl_mixer/*:mpg123": False,
+        "sdl_mixer/*:nativemidi": False,
+        "sdl_mixer/*:ogg": True,
+        "sdl_mixer/*:opus": False,
+        "sdl_mixer/*:tinymidi": False,
+        "sdl_mixer/*:wav": False,
+    }
 
     def build_requirements(self):
         self.test_requires("gtest/1.13.0")
