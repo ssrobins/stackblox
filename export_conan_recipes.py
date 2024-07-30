f02e7f4376f4ed81eb38f1a3ea51e7f77ad67132
Steve Robinson <ssrobins@gmail.com>
2024-07-29 19:38:58 -0700

    Update Android version codes

--- export_conan_recipes.py
+++ export_conan_recipes.py
@@ -17,7 +17,7 @@ subprocess.run(["git", "fetch", "origin", conan_center_branch],
 subprocess.run(["git", "reset", "--hard", f"origin/{conan_center_branch}"],
     check=True, cwd=conan_center_dir)
 
-subprocess.run(["conan", "export", ".", "--version", "2.26.5"],
+subprocess.run(["conan", "export", ".", "--version", "2.30.4"],
     check=True, cwd=f"{conan_center_dir}/recipes/sdl/all")
 subprocess.run(["conan", "export", ".", "--version", "2.0.5"],
     check=True, cwd=f"{conan_center_dir}/recipes/sdl_image/all")
